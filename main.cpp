369323aff81af2234f77fa3c127cd6d295e58d9f
TealSmurf <jcoin707@gmail.com>
2024-03-08 08:47:05 -0800

    Created program that takes input for dimensions of square which has pairs of numbers. Example of 3x3 0, 0; 0, 1; 0, 2 1, 0; 1, 1; 1, 2 2, 0; 2, 1; 2, 2 The program prints out the pairs of numbers in the bottom right triangle of the square including the middle diagonal.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@ using namespace std;
 int main()
 {
 	int N;
+	cout << "Enter dimensions of the square: ";
 	cin >> N;
 
-	/***************************************************
-	 * Code Your program here
-	 ***************************************************/
+	for(int i = 0; i < N; i++)
+	{
+		int a = N;
+		for(int j = 0; j < N; j++)
+		{
+			if(a-i>1)
+			{
+				a--;
+			}
+			else if(0<a<=1)
+			{
+				cout << i << ", " << j << endl;
+			}
+		}
+
+	}
 }
